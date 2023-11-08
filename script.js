0b30f1f6c58496596976667af6b5ca5a70a70cab
ClickForShinnyRocks <150286045+ClickForShinnyRocks@users.noreply.github.com>
2023-11-08 13:21:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
+var _0xad1d6c = function () {
+  var _0x2106ac = true;
+  return function (_0xc0bce0, _0x180912) {
+    var _0x536cad = _0x2106ac ? function () {
+      if (_0x180912) {
+        var _0x293136 = _0x180912.apply(_0xc0bce0, arguments);
+        _0x180912 = null;
+        return _0x293136;
+      }
+    } : function () {};
+    _0x2106ac = false;
+    return _0x536cad;
+  };
+}();
+(function () {
+  _0xad1d6c(this, function () {
+    var _0xf5cd2e = new RegExp("function *\\( *\\)");
+    var _0x435a5d = new RegExp("\\+\\+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)", "i");
+    var _0x58e0d9 = _0x3cba60("init");
+    if (!_0xf5cd2e.test(_0x58e0d9 + "chain") || !_0x435a5d.test(_0x58e0d9 + "input")) {
+      _0x58e0d9("0");
+    } else {
+      _0x3cba60();
+    }
+  })();
+})();
+var _0x3e23d0 = function () {
+  var _0x20cb29 = true;
+  return function (_0x5a6832, _0x46a789) {
+    var _0xc2d50d = _0x20cb29 ? function () {
+      if (_0x46a789) {
+        var _0x588dd6 = _0x46a789.apply(_0x5a6832, arguments);
+        _0x46a789 = null;
+        return _0x588dd6;
+      }
+    } : function () {};
+    _0x20cb29 = false;
+    return _0xc2d50d;
+  };
+}();
+var _0x3cabf9 = _0x3e23d0(this, function () {
+  var _0x390340;
+  try {
+    var _0x27cb1d = Function("return (function() {}.constructor(\"return this\")( ));");
+    _0x390340 = _0x27cb1d();
+  } catch (_0x5a06c5) {
+    _0x390340 = window;
+  }
+  var _0x2b44a5 = _0x390340.console = _0x390340.console || {};
+  var _0x101758 = ["log", "warn", "info", "error", "exception", "table", "trace"];
+  for (var _0x5d90b0 = 0; _0x5d90b0 < _0x101758.length; _0x5d90b0++) {
+    var _0x2695de = 0;
+    var _0x426d8e = _0x3e23d0.constructor.prototype.bind(_0x3e23d0);
+    var _0x5f513c = _0x101758[_0x5d90b0];
+    var _0x32878d = _0x2b44a5[_0x5f513c] || _0x426d8e;
+    _0x426d8e.__proto__ = _0x3e23d0.bind(_0x3e23d0);
+    _0x426d8e.toString = _0x32878d.toString.bind(_0x32878d);
+    _0x2b44a5[_0x5f513c] = _0x426d8e;
+  }
+});
 _0x3cabf9();
 var _0x33847d = new XMLHttpRequest();
 const _0x39f6bf = ["femboykisser", "femboylover", "nigger", "nigga", "ilovefemboys", "ihatevlackies", "blickpeople", "banlemming", "lemmingisanig", "niggerman22", "OCULUS SEX", "OCULUSFUCKLEMMING", "OCULUSNIGERIA"];
